b29c781c7d3e4395c99ac1b56c357b1a3f2f6d5c
Gastón Piedra <piedragaston55@gmail.com>
2023-08-13 01:43:44 -0300

    Final

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ function mostrarSaludo() {
     alert("Hola!");
 }
 document.getElementById("saludarBtn").onclick = mostrarSaludo;
+
+const divContainer = document.getElementById("btnContainer");
+
+divContainer.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
